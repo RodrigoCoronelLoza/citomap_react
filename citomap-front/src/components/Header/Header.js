76860429c801e6baec505d4a44719c3dbacb01a1
RodrigoCoronelLoza <drigo31416@gmail.com>
2023-10-23 10:49:48 -0400

    header init

---
+++ citomap-front/src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./header.module.css";
+
+export default function Header() {
+  const user = {
+    name: "Jhon",
+  };
+  const cart = {
+    totalCount: 10,
+  };
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+          CITOMAP
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/information">Quienes Somos</Link>
+            </li>
+
+            <li>
+              <Link to="/portafolio">Portafolio</Link>
+            </li>
+
+            <li>
+              <Link to="/location">Contactanos</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
